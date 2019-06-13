bc4d46383f70ad2860692b617c73b26997ef3b0b
wkkk <kysoowjk@163.com>
2019-06-13 16:01:23 +0800

    :bug: 修改了一些语法以适应c++特性，控制了block大小和node大小

--- IndexManager/bplusTree.h
+++ IndexManager/bplusTree.h
@@ -11,14 +11,14 @@ using namespace std;
 #define blockSize 4096 // 块大小规定为4KB，这里和缓冲区相同
 #define nodeSize 3
 #define INT_MAX 1000000
-#define MAX 1000
+#define MAX 300 // Math.floor((4096-4-4)/12)
 
 class bNode
 {
 public:
     int size;
     bNode *parentNode;
-    bNode *childNode[MAX]; // 从0开始，共nodeSize个基本单位
+    bNode *childNode[MAX]; // 从0开始，共MAX个基本单位
     pair<int,int> value[MAX]; // first:key; second:addr/offset
 
     bNode();
@@ -28,19 +28,16 @@ public:
 bNode::bNode()
 {
     size = 0;
-    parentNode = NULL;
+    parentNode = nullptr;
     for (int i = 0; i < MAX; i++)
     {
         value[i] = make_pair(INT_MAX,-1);
-        childNode[i] = NULL;
+        childNode[i] = nullptr;
     }
 }
 bool bNode::isLeaf()
 {
-    if (childNode[0] == NULL)
-        return true;
-    else
-        return false;
+    return childNode[0] == nullptr;
 }
 
 class bplusTree
@@ -65,41 +62,45 @@ public:
     void redistributeNode(bNode *leftNode, bNode *rightNode, bool isLeaf, int posOfLeftNode, int whichOneisCurNode);
     int getAddrWithKey(bNode *curNode, int key);
 
-    void print(vector<bNode *> Nodes); // 打印树结构，调试用
+    static void print(vector<bNode *> Nodes); // 打印树结构，调试用
 };
 
 bplusTree::bplusTree()
 {
     numberOfPointers = nodeSize + 1;
     root = new bNode();
+    searchDataFound = false;
+    dataFound = false;
 };
 
 bplusTree::bplusTree(string tableName, string keyName)
 {
     this->tableName = tableName;
     this->keyName = keyName;
+    root = new bNode();
     numberOfPointers = nodeSize + 1;
+    searchDataFound = false;
+    dataFound = false;
 }
 
 void bplusTree::print(vector<bNode *> Nodes)
 {
     vector<bNode *> newNodes;
-    for (int i = 0; i < Nodes.size(); i++)
+    for (auto curNode : Nodes)
     {
-        bNode *curNode = Nodes[i];
         cout << "[|";
         int j;
         for (j = 0; j < curNode->size; j++)
         {
             cout << curNode->value[j].first<<":"<<curNode->value[j].second << "|";
-            if (curNode->childNode[j] != NULL)
+            if (curNode->childNode[j] != nullptr)
                 newNodes.push_back(curNode->childNode[j]);
         }
-        if (curNode->value[j].first == INT_MAX && curNode->childNode[j] != NULL)
+        if (curNode->value[j].first == INT_MAX && curNode->childNode[j] != nullptr)
             newNodes.push_back(curNode->childNode[j]);
         cout << "]  ";
     }
-    if (newNodes.size() == 0)
+    if (newNodes.empty())
     {
 
         puts("");
@@ -126,7 +127,7 @@ void bplusTree::splitLeaf(bNode *curNode)
     else
         x = numberOfPointers / 2;
 
-    bNode *rightNode = new bNode();
+    auto *rightNode = new bNode();
 
     curNode->size = x;
     rightNode->size = numberOfPointers - x;
@@ -139,10 +140,10 @@ void bplusTree::splitLeaf(bNode *curNode)
     }
     int val = rightNode->value[0].first;
     int address = rightNode->value[0].second;
-    if (curNode->parentNode == NULL)
+    if (curNode->parentNode == nullptr)
     {
-        bNode *parentNode = new bNode();
-        parentNode->parentNode = NULL;
+        auto *parentNode = new bNode();
+        parentNode->parentNode = nullptr;
         parentNode->size = 1;
         parentNode->value[0] = rightNode->value[0];
         parentNode->childNode[0] = curNode;
@@ -154,7 +155,7 @@ void bplusTree::splitLeaf(bNode *curNode)
     else
     {
         curNode = curNode->parentNode;
-        bNode *newChildNode = new bNode();
+        auto *newChildNode = new bNode();
         newChildNode = rightNode;
         tempPair = make_pair(val,address);
         for (i = 0; i <= curNode->size; i++)
@@ -173,7 +174,7 @@ void bplusTree::splitLeaf(bNode *curNode)
             }
         }
         curNode->childNode[i] = newChildNode;
-        for (i = 0; curNode->childNode[i] != NULL; i++)
+        for (i = 0; curNode->childNode[i] != nullptr; i++)
         {
             curNode->childNode[i]->parentNode = curNode;
         }
@@ -187,7 +188,7 @@ void bplusTree::splitNonLeaf(bNode *curNode)
     pair<int,int> tempPair;
 
     x = numberOfPointers / 2;
-    bNode *rightNode = new bNode();
+    auto *rightNode = new bNode();
     curNode->size = x;
     rightNode->size = numberOfPointers - x - 1;
     rightNode->parentNode = curNode->parentNode;
@@ -198,26 +199,26 @@ void bplusTree::splitNonLeaf(bNode *curNode)
         rightNode->childNode[j] = curNode->childNode[i];
         curNode->value[i] = make_pair(INT_MAX,-1);
         if (i != x)
-            curNode->childNode[i] = NULL;
+            curNode->childNode[i] = nullptr;
     }
     int val = rightNode->value[0].first;
     int address = rightNode->value[0].second;
     memcpy(&rightNode->value, &rightNode->value[1], sizeof(int) * (rightNode->size + 1));
     memcpy(&rightNode->childNode, &rightNode->childNode[1], sizeof(root) * (rightNode->size + 1));
 
-    for (i = 0; curNode->childNode[i] != NULL; i++)
+    for (i = 0; curNode->childNode[i] != nullptr; i++)
     {
         curNode->childNode[i]->parentNode = curNode;
     }
-    for (i = 0; rightNode->childNode[i] != NULL; i++)
+    for (i = 0; rightNode->childNode[i] != nullptr; i++)
     {
         rightNode->childNode[i]->parentNode = rightNode;
     }
 
-    if (curNode->parentNode == NULL)
+    if (curNode->parentNode == nullptr)
     {
-        bNode *parentNode = new bNode();
-        parentNode->parentNode = NULL;
+        auto *parentNode = new bNode();
+        parentNode->parentNode = nullptr;
         parentNode->size = 1;
         parentNode->value[0] = make_pair(val,address);
         parentNode->childNode[0] = curNode;
@@ -229,8 +230,8 @@ void bplusTree::splitNonLeaf(bNode *curNode)
     else
     {
         curNode = curNode->parentNode;
-        bNode *newchildNode = new bNode();
-        newchildNode = rightNode;
+        auto *newChildNode = new bNode();
+        newChildNode = rightNode;
         tempPair = make_pair(val,address);
         for (i = 0; i <= curNode->size; i++)
         {
@@ -242,13 +243,13 @@ void bplusTree::splitNonLeaf(bNode *curNode)
         curNode->size++;
         for (i = 0; i < curNode->size; i++)
         {
-            if (newchildNode->value[0] < curNode->childNode[i]->value[0])
+            if (newChildNode->value[0] < curNode->childNode[i]->value[0])
             {
-                swap(curNode->childNode[i], newchildNode);
+                swap(curNode->childNode[i], newChildNode);
             }
         }
-        curNode->childNode[i] = newchildNode;
-        for (i = 0; curNode->childNode[i] != NULL; i++)
+        curNode->childNode[i] = newChildNode;
+        for (i = 0; curNode->childNode[i] != nullptr; i++)
         {
             curNode->childNode[i]->parentNode = curNode;
         }
@@ -261,14 +262,14 @@ void bplusTree::insertNode(bNode *curNode, int key, int addr)
     pair<int,int> tempPair = make_pair(key,addr);
     for (int i = 0; i <= curNode->size; i++)
     {
-        if (key < curNode->value[i].first && curNode->childNode[i] != NULL)
+        if (key < curNode->value[i].first && curNode->childNode[i] != nullptr)
         {
             insertNode(curNode->childNode[i], key,addr);
             if (curNode->size == numberOfPointers)
                 splitNonLeaf(curNode);
             return;
         }
-        else if (key < curNode->value[i].first && curNode->childNode[i] == NULL)
+        else if (key < curNode->value[i].first && curNode->childNode[i] == nullptr)
         {
             swap(curNode->value[i], tempPair);
             if (i == curNode->size)
@@ -286,8 +287,6 @@ void bplusTree::insertNode(bNode *curNode, int key, int addr)
 
 void bplusTree::redistributeNode(bNode *leftNode, bNode *rightNode, bool isLeaf, int posOfLeftNode, int whichOneisCurNode)
 {
-    int PrevRightFirstVal = rightNode->value[0].first;
-
     if (whichOneisCurNode == 0)
     {
         if (!isLeaf)
@@ -320,7 +319,7 @@ void bplusTree::redistributeNode(bNode *leftNode, bNode *rightNode, bool isLeaf,
             rightNode->size++;
             leftNode->parentNode->value[posOfLeftNode] = leftNode->value[leftNode->size - 1];
             leftNode->value[leftNode->size - 1] = make_pair(INT_MAX,-1);
-            leftNode->childNode[leftNode->size] = NULL;
+            leftNode->childNode[leftNode->size] = nullptr;
             leftNode->size--;
         }
         else
@@ -349,7 +348,7 @@ void bplusTree::mergeNode(bNode *leftNode, bNode *rightNode, bool isLeaf, int po
     memcpy(&leftNode->parentNode->value[posOfRightNode - 1], &leftNode->parentNode->value[posOfRightNode], sizeof(int) * (leftNode->parentNode->size + 1));
     memcpy(&leftNode->parentNode->childNode[posOfRightNode], &leftNode->parentNode->childNode[posOfRightNode + 1], sizeof(root) * (leftNode->parentNode->size + 1));
     leftNode->parentNode->size--;
-    for (int i = 0; leftNode->childNode[i] != NULL; i++)
+    for (int i = 0; leftNode->childNode[i] != nullptr; i++)
     {
         leftNode->childNode[i]->parentNode = leftNode;
     }
@@ -360,13 +359,13 @@ void bplusTree::deleteNode(bNode *curNode, int key, int curNodePosition)
     bool isLeaf = curNode->isLeaf();
     int prevLeftMostVal = curNode->value[0].first;
 
-    for (int i = 0; dataFound == false && i <= curNode->size; i++)
+    for (int i = 0; !dataFound && i <= curNode->size; i++)
     {
-        if (key < curNode->value[i].first && curNode->childNode[i] != NULL)
+        if (key < curNode->value[i].first && curNode->childNode[i] != nullptr)
         {
             deleteNode(curNode->childNode[i], key, i);
         }
-        else if (key == curNode->value[i].first && curNode->childNode[i] == NULL)
+        else if (key == curNode->value[i].first && curNode->childNode[i] == nullptr)
         {
             memcpy(&curNode->value[i], &curNode->value[i + 1], sizeof(int) * (curNode->size + 1));
             curNode->size--;
@@ -374,98 +373,98 @@ void bplusTree::deleteNode(bNode *curNode, int key, int curNodePosition)
             break;
         }
     }
-    if (curNode->parentNode == NULL && curNode->childNode[0] == NULL)
+    if (curNode->parentNode == nullptr && curNode->childNode[0] == nullptr)
     {
         return;
     }
-    if (curNode->parentNode == NULL && curNode->childNode[0] != NULL && curNode->size == 0)
+    if (curNode->parentNode == nullptr && curNode->childNode[0] != nullptr && curNode->size == 0)
     {
         root = curNode->childNode[0];
-        root->parentNode = NULL;
+        root->parentNode = nullptr;
         return;
     }
-    if (isLeaf && curNode->parentNode != NULL)
+    if (isLeaf && curNode->parentNode != nullptr)
     {
         if (curNodePosition == 0)
         {
-            bNode *rightNode = new bNode();
+            auto *rightNode = new bNode();
             rightNode = curNode->parentNode->childNode[1];
-            if (rightNode != NULL && rightNode->size > (numberOfPointers + 1) / 2)
+            if (rightNode != nullptr && rightNode->size > (numberOfPointers + 1) / 2)
             {
                 redistributeNode(curNode, rightNode, isLeaf, 0, 0);
             }
-            else if (rightNode != NULL && curNode->size + rightNode->size < numberOfPointers)
+            else if (rightNode != nullptr && curNode->size + rightNode->size < numberOfPointers)
             {
                 mergeNode(curNode, rightNode, isLeaf, 1);
             }
         }
         else
         {
-            bNode *leftNode = new bNode();
-            bNode *rightNode = new bNode();
+            auto *leftNode = new bNode();
+            auto *rightNode = new bNode();
             leftNode = curNode->parentNode->childNode[curNodePosition - 1];
             rightNode = curNode->parentNode->childNode[curNodePosition + 1];
-            if (leftNode != NULL && leftNode->size > (numberOfPointers + 1) / 2)
+            if (leftNode != nullptr && leftNode->size > (numberOfPointers + 1) / 2)
             {
                 redistributeNode(leftNode, curNode, isLeaf, curNodePosition - 1, 1);
             }
-            else if (rightNode != NULL && rightNode->size > (numberOfPointers + 1) / 2)
+            else if (rightNode != nullptr && rightNode->size > (numberOfPointers + 1) / 2)
             {
                 redistributeNode(curNode, rightNode, isLeaf, curNodePosition, 0);
             }
-            else if (leftNode != NULL && curNode->size + leftNode->size < numberOfPointers)
+            else if (leftNode != nullptr && curNode->size + leftNode->size < numberOfPointers)
             {
                 mergeNode(leftNode, curNode, isLeaf, curNodePosition);
             }
-            else if (rightNode != NULL && curNode->size + rightNode->size < numberOfPointers)
+            else if (rightNode != nullptr && curNode->size + rightNode->size < numberOfPointers)
             {
                 mergeNode(curNode, rightNode, isLeaf, curNodePosition + 1);
             }
         }
     }
-    else if (!isLeaf && curNode->parentNode != NULL)
+    else if (!isLeaf && curNode->parentNode != nullptr)
     {
 
         if (curNodePosition == 0)
         {
-            bNode *rightNode = new bNode();
+            auto *rightNode = new bNode();
             rightNode = curNode->parentNode->childNode[1];
-            if (rightNode != NULL && rightNode->size - 1 >= ceil((numberOfPointers - 1) / 2))
+            if (rightNode != nullptr && rightNode->size - 1 >= ceil((numberOfPointers - 1) / 2))
             {
                 redistributeNode(curNode, rightNode, isLeaf, 0, 0);
             }
-            else if (rightNode != NULL && curNode->size + rightNode->size < numberOfPointers - 1)
+            else if (rightNode != nullptr && curNode->size + rightNode->size < numberOfPointers - 1)
             {
                 mergeNode(curNode, rightNode, isLeaf, 1);
             }
         }
         else
         {
-            bNode *leftNode = new bNode();
-            bNode *rightNode = new bNode();
+            auto *leftNode = new bNode();
+            auto *rightNode = new bNode();
             leftNode = curNode->parentNode->childNode[curNodePosition - 1];
             rightNode = curNode->parentNode->childNode[curNodePosition + 1];
-            if (leftNode != NULL && leftNode->size - 1 >= ceil((numberOfPointers - 1) / 2))
+            if (leftNode != nullptr && leftNode->size - 1 >= ceil((numberOfPointers - 1) / 2))
             {
                 redistributeNode(leftNode, curNode, isLeaf, curNodePosition - 1, 1);
             }
-            else if (rightNode != NULL && rightNode->size - 1 >= ceil((numberOfPointers - 1) / 2))
+            else if (rightNode != nullptr && rightNode->size - 1 >= ceil((numberOfPointers - 1) / 2))
             {
                 redistributeNode(curNode, rightNode, isLeaf, curNodePosition, 0);
             }
-            else if (leftNode != NULL && curNode->size + leftNode->size < numberOfPointers - 1)
+            else if (leftNode != nullptr && curNode->size + leftNode->size < numberOfPointers - 1)
             {
                 mergeNode(leftNode, curNode, isLeaf, curNodePosition);
             }
-            else if (rightNode != NULL && curNode->size + rightNode->size < numberOfPointers - 1)
+            else if (rightNode != nullptr && curNode->size + rightNode->size < numberOfPointers - 1)
             {
                 mergeNode(curNode, rightNode, isLeaf, curNodePosition + 1);
             }
         }
     }
-    bNode *tempNode = new bNode();
+    auto *tempNode = new bNode();
     tempNode = curNode->parentNode;
-    while (tempNode != NULL)
+    while (tempNode != nullptr)
     {
         for (int i = 0; i < tempNode->size; i++)
         {
